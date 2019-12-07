217e6ad60b377e60c76bc26ee3b2b0d8f5446323
szykol <szymonkolton@gmail.com>
2019-12-07 11:14:31 +0100

    Add tests to the project

---
+++ src/test_endpoints.py
@@ -0,0 +1,33 @@
+import unittest
+import subprocess
+import requests
+import time
+import pytest
+from urllib3.exceptions import NewConnectionError
+
+API_PORT=8000
+
+class EndpointTest(unittest.TestCase):
+    def setUp(self):
+        self.sb = subprocess.Popen('/usr/api/src/server/server.py')
+        self._wait_for_port()
+
+    def tearDown(self):
+        self.sb.kill()
+
+    def _wait_for_port(self, timeout=5, port=API_PORT):
+        tout = time.monotonic() + timeout
+
+        while time.monotonic() < tout:
+            try:
+                requests.get('http://localhost:{}/'.format(port))
+            except Exception:
+                time.sleep(0.1)
+            else:
+                return
+        pytest.fail()
+
+
+    def test_root(self):
+        r = requests.get('http://localhost:8000/')
+        print(r)
--- src/validate.py
+++
@@ -1,5 +0,0 @@
-def validate_user(data):
-    has_key_not_empty = lambda key: key in data and data[key]
-
-    keys = ['name', 'surname', 'email', 'password']
-    return all(has_key_not_empty(k) for k in keys)
